6f51e2e1bcae59ed5a831756fa10a3cb75f18814
Hamin <21300786@handong.edu>
2018-07-19 22:04:36 +0900

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: const Text('Wordpress Cafe'),
+        backgroundColor: Color.fromRGBO(0, 0, 0, 0.0),
+      ),
+    );
+  }
+}
